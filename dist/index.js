ae6b223847e72009bb92f8c6d5a83e58c7986576
Nkmeoo7 <nitin.22437@knit.ac.in>
2025-09-22 03:12:35 +0530

    removed the dist floder

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import { WebSocketServer } from "ws";
-const wss = new WebSocketServer({ port: 8080 });
-let userConnected = 0;
-//
-wss.on("connection", (socket) => {
-    userConnected++;
-    console.log('user connected' + userConnected);
-});
-//# sourceMappingURL=index.js.map
